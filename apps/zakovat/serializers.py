46d0692f5b60991e90b21b08dca6f9b8a48daeb3
Abdusamad Malikov <abdusamad@kali.abdusamad>
2022-11-05 15:12:54 -0400

    `Members` api created

--- apps/zakovat/serializers.py
+++ apps/zakovat/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from apps.zakovat.models import Team
+from apps.zakovat.models import Team, Member
 
 
 class TeamSerializer(serializers.ModelSerializer):
@@ -9,3 +9,18 @@ class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
         fields = ['id', 'name', 'leader', 'members', 'image', 'description']
+
+
+class MemberSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+    team = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Member
+        fields = ['full_name', 'team']
+    
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+    
+    def get_team(self, obj):
+        return obj.teams.values_list('name', flat=True)
--- apps/zakovat/views.py
+++ apps/zakovat/views.py
@@ -1,9 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
-from apps.zakovat.models import Team
-from apps.zakovat.serializers import TeamSerializer
+from apps.zakovat.models import Team, Member
+from apps.zakovat.serializers import TeamSerializer, MemberSerializer
 from typing import List
 
 class TeamViewSet(ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
+    http_method_names: List[str] = ['get', 'head', 'options']
+
+
+class MemberViewSet(ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
     http_method_names: List[str] = ['get', 'head', 'options']
