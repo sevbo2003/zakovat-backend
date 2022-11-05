bfdef844a1908f486f87b14c4464062f84443ad8
Abdusamad Malikov <abdusamad@kali.abdusamad>
2022-11-05 17:22:58 -0400

    `user admin` created

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from apps.authentication.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email','username', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('email','username', 'first_name', 'last_name', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'username')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'username', 'last_name', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email','username', 'first_name', 'last_name')
+    ordering = ('email',)
+
+
+admin.site.register(User, CustomUserAdmin)
