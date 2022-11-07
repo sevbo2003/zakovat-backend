b9284230a38cdd0c882b64cea7a225652d540a3b
Abdusamad Malikov <abdusamad@kali.abdusamad>
2022-11-07 11:10:22 -0500

    `game filters` implemented

---
+++ apps/event/filters.py
@@ -0,0 +1,9 @@
+from apps.event.models import Game
+import django_filters
+
+
+class GameFilter(django_filters.FilterSet):
+    group = django_filters.CharFilter(field_name='group__name')
+    class Meta:
+        model = Game
+        fields = ['group', 'team1', 'team2','is_finished']
--- apps/event/views.py
+++ apps/event/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from apps.event.serializers import GroupSerializer, GameSerializer
 from apps.event.models import Group, Game, Result
+from django_filters.rest_framework import DjangoFilterBackend
+from apps.event.filters import GameFilter
 
 
 class GroupViewSet(ModelViewSet):
@@ -13,3 +15,5 @@ class GameViewSet(ModelViewSet):
     queryset = Game.objects.all().order_by('-time')
     serializer_class = GameSerializer
     http_method_names = ['get', 'head', 'options']
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = GameFilter
