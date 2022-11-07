ce577d5975ff2a4894de1e9d20b3a0b5c6d9840e
Abdusamad Malikov <abdusamad@kali.abdusamad>
2022-11-07 13:25:51 -0500

    `action` decorator added

--- apps/zakovat/views.py
+++ apps/zakovat/views.py
@@ -1,12 +1,58 @@
 from rest_framework.viewsets import ModelViewSet
 from apps.zakovat.models import Team, Member
 from apps.zakovat.serializers import TeamSerializer, MemberSerializer
+from apps.event.serializers import ResultSerializer
 from typing import List
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+
+
 
 class TeamViewSet(ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
     http_method_names: List[str] = ['get', 'head', 'options']
+
+    @action(detail=True, methods=['get'], url_name='past-results')
+    def nim_chorak_results(self, request, pk=None):
+        team = self.get_object()
+        query = team.result_set.all() | team.loser.all()
+        query = query.order_by("game__time")
+        serializer = ResultSerializer(query, many=True)
+        return Response({
+            "team": {
+                "id": team.id,
+                "name": team.name
+            },
+            "results": serializer.data
+            }, status=status.HTTP_200_OK)
+    
+    @action(detail=True, methods=['get'])
+    def wins(self, request, pk=None):
+        team = self.get_object()
+        results = team.result_set.all()
+        serializer = ResultSerializer(results, many=True)
+        return Response({
+            "team": {
+                "id": team.id,
+                "name": team.name
+            },
+            "results": serializer.data
+            }, status=status.HTTP_200_OK)
+    
+    @action(detail=True, methods=['get'])
+    def loses(self, request, pk=None):
+        team = self.get_object()
+        results = team.loser.all()
+        serializer = ResultSerializer(results, many=True)
+        return Response({
+            "team": {
+                "id": team.id,
+                "name": team.name
+            },
+            "results": serializer.data
+            }, status=status.HTTP_200_OK)
 
 
 class MemberViewSet(ModelViewSet):
