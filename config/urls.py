cdfa3091fd2e96f66c7ca05625b964b74065cdfc
Abdusamad Malikov <abdusamad@kali.abdusamad>
2022-11-09 07:33:38 -0500

    `sexy` girl

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,5 @@ urlpatterns = [
     path('api/v1/', include(router.urls)),
 ]
 
-if settings.DEBUG==False:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
