dd7af43367d21864b2c0b0432c460d1e255ee4eb
Abdusamad Malikov <abdusamad@kali.abdusamad>
2022-11-09 07:21:03 -0500

    `settings` debug setted

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(' ')
 
